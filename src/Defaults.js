6b1d5e4d086c3d9707768f229beded1c42235a4d
Jacob M. Roufa <jacob.roufa@gmail.com>
2018-08-01 19:11:28 -0500

    add defaults for bass, guitar, and mandolin

---
+++ src/Defaults.js
@@ -0,0 +1,18 @@
+const presets = {
+	bass: {
+		strings: 4,
+		frets: 20,
+		notes: [ "G", "D", "A", "E" ]
+	},
+	guitar: {
+		strings: 6,
+		frets: 24,
+		notes: [ "E", "B", "G", "D", "A", "E" ]
+	},
+	mandolin: {
+		strings: 4,
+		frets: 22,
+		notes: [ "E", "A", "D", "G" ],
+		octave: true
+	}
+};
