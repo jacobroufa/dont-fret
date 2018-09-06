759f34dd31993067d7d3c5fdac6158ef925c864b
Jacob M. Roufa <jacob.roufa@gmail.com>
2018-09-05 20:50:08 -0500

    get a mostly working fretboard

--- src/Defaults.js
+++ src/Defaults.js
@@ -23,3 +23,36 @@ export const instruments = {
 
 export const FRET_WIDTH = 40;
 export const STRING_WIDTH = 2;
+
+export const LEFT_MARGIN = 20;
+
+export const FRET_SIZE = 3;
+export const NUT_SIZE = 6;
+
+// taken from https://www.liutaiomottola.com/formulae/fret.htm
+export const FRET_CONSTANT = [
+	0.056126,
+	0.109101,
+	0.159104,
+	0.206291,
+	0.250847,
+	0.292893,
+	0.33258,
+	0.370039,
+	0.405396,
+	0.438769,
+	0.470268,
+	0.5,
+	0.528063,
+	0.554551,
+	0.579552,
+	0.60315,
+	0.625423,
+	0.646447,
+	0.66629,
+	0.68502,
+	0.702698,
+	0.719385,
+	0.735134,
+	0.75
+];
--- src/Fretboard.js
+++ src/Fretboard.js
@@ -1,4 +1,12 @@
-import { FRET_WIDTH, instruments, STRING_WIDTH } from "./Defaults.js";
+import {
+	instruments,
+	FRET_CONSTANT,
+	FRET_SIZE,
+	FRET_WIDTH,
+	LEFT_MARGIN,
+	NUT_SIZE,
+	STRING_WIDTH
+} from "./Defaults.js";
 import { getWidth } from "./Utils.js";
 
 export default class Fretboard {
@@ -21,13 +29,40 @@ export default class Fretboard {
 		document.body.addEventListener("resize", this.setFretboardSize);
 	}
 
+	redrawFrets() {
+		const instrument = this.getInstrument();
+		let drawnFrets = 0;
+		let leftMargin = LEFT_MARGIN * 2;
+
+		this.fretboard
+			.rect(NUT_SIZE, this.height)
+			.move(leftMargin, 0);
+
+		leftMargin += NUT_SIZE;
+
+		while (drawnFrets < instrument.frets) {
+			const fretConstant = FRET_CONSTANT[drawnFrets];
+			const fretBoard = this.width - leftMargin;
+			const scaleLength = (fretBoard / 3) * 4;
+			const fretDistance = fretConstant * scaleLength;
+
+			// leftMargin += fretDistance;
+
+			this.fretboard
+				.rect(FRET_SIZE, this.height)
+				.move(fretDistance, 0);
+
+			// leftMargin += NUT_SIZE;
+
+			drawnFrets++;
+		}
+	}
+
 	redrawStrings() {
 		const half = (FRET_WIDTH - STRING_WIDTH) / 2;
 		const stringHalf = half + STRING_WIDTH;
 		const instrument = this.getInstrument();
 		let rects = 1;
-
-		this.fretboard.clear();
 
 		instrument.notes.forEach((note, index) => {
 			const rect = index + 1;
@@ -38,13 +73,15 @@ export default class Fretboard {
 			}
 
 			this.fretboard
-				.rect(this.width, STRING_WIDTH)
+				.rect(this.width - LEFT_MARGIN, STRING_WIDTH)
 				.attr({ fill: "#333" })
-				.move(0, heightDiff);
+				.move(LEFT_MARGIN, heightDiff);
 
-			this.fretboard
-				.text(note)
-				.move(0, heightDiff);
+			const text = this.fretboard
+				.text(note);
+			const fontHeight = text.font("size") / 2;
+
+			text.move(0, heightDiff - fontHeight);
 		});
 	}
 
@@ -57,12 +94,15 @@ export default class Fretboard {
 	}
 
 	setFretboardSize() {
-		this.width = getWidth();
-
 		const instrument = this.getInstrument();
 
-		this.fretboard.size(this.width, instrument.strings * FRET_WIDTH);
+		this.width = getWidth();
+		this.height = instrument.strings * FRET_WIDTH;
 
+		this.fretboard.clear();
+		this.fretboard.size(this.width, this.height);
+
+		this.redrawFrets();
 		this.redrawStrings();
 	}
 }
